9b242ea4af565df98e2aa4b61ea038e2dd5ec9d0
chanuka <chanukawelagedara@gmail.com>
2024-03-09 20:01:51 +0530

    4rth commit
    edited

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -2,11 +2,11 @@ import express from "express";
 import * as userCtrl from "../controller/userCtrl.js";
 const router = express.Router();
 
-router.post("/register",userCtrl.createUser);
-router.post("/login",userCtrl.login);
-router.get('/:id',userCtrl.getUserById);
-router.put('/:id',userCtrl.editUser);
-router.delete('/:id',userCtrl.deleteUser);
+// router.post("/register",userCtrl.createUser);
+// router.post("/login",userCtrl.login);
+// router.get('/:id',userCtrl.getUserById);
+// router.put('/:id',userCtrl.editUser);
+// router.delete('/:id',userCtrl.deleteUser);
 router.post('/firebase-auth', userCtrl.authenticateWithFirebase);
 //router.post('/signup', userCtrl.signUpWithFirebase);
 router.post('/logout',userCtrl.revokeAccessToken)
