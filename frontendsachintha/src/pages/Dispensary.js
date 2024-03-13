5b4ee957c0c4cf05cc2ba8671e296332302ae12c
sachintharx <139696727+sachintharx@users.noreply.github.com>
2024-03-13 15:41:27 +0530

    03

--- frontendsachintha/src/pages/Dispensary.js
+++ frontendsachintha/src/pages/Dispensary.js
@@ -1,9 +1,14 @@
 import React from "react";
+import DispensaryTop from "../components/Dispensary/Dispensary_top";
+
+import Slider from "../components/Dispensary/Slider";
 
 
 export default function Dispensary () {
     return (
         <div>
+            <DispensaryTop/>
+            <Slider/>
             
         </div>
     );
