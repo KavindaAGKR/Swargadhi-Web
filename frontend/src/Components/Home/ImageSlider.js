83b10e49617bcc1ea15d300d565707fecce1f760
KavindaAGKR <kavindaaludeniya@gmail.com>
2024-03-15 16:59:41 +0530

    Image slider Edited

--- frontend/src/Components/Home/ImageSlider.js
+++ frontend/src/Components/Home/ImageSlider.js
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './ImageSlider.css';
 
-const images = [
-  'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQYALwFaLdWRO4aMfsfPYdnr2vD73VuUr0y1tTiFjYENDXf7XBEIqdYbX1TUPcNy44sYx1wMRrgSM6nBZM4QSU',
-  'http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRvC27D9KlxeEham1w-Wpd_pu3hd4A-OywxRbdnx9JFLpcTD7dfL0bD_WI6Ro8QkzrPLkBMzA9osrMpi4JSP5Y',
-  'http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSVby3wf1HYF5NReffKSwxLbZRENM0zRUaM97yz5OYBiJGgFHsq0aVo_iE7di8yzBEW4Z3eEjcR5avNEeF7xv4',
-];
 
 
 function ImageSlider({ images, imageSize, slideInterval }) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+              const intervalId = setInterval(() => {
+                setCurrentImageIndex((prevIndex) =>
+                  prevIndex === images.length - 1 ? 0 : prevIndex + 1
+                );
+              }, slideInterval);
+            
+              return () => clearInterval(intervalId);
+            }, 
+            [images, slideInterval]
       );
-    }, slideInterval);
 
-    return () => clearInterval(intervalId);
-  }, [images, slideInterval]);
+
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) =>
@@ -27,12 +26,15 @@ function ImageSlider({ images, imageSize, slideInterval }) {
     );
   };
 
+
   const prevImage = () => {
     setCurrentImageIndex((prevIndex) =>
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
 
+
+  
   return (
     <div className="image-slider">
       <div className="image-container">
