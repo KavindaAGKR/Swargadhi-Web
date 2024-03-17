6c8aa5088be7ffb3d23ae7d4b2794caf3d9065b2
chanuka <chanukawelagedara@gmail.com>
2024-03-17 17:37:56 +0530

    new

--- frontend/src/AdminPanel/AdminDashboard.js
+++ frontend/src/AdminPanel/AdminDashboard.js
@@ -24,25 +24,6 @@ export const AdminDashboard = () => {
 
     return (
         <Grid container>
-            {/* <Grid item xs={3}>
-                <Box gap='50px' sx={{ backgroundColor: '#A1FEC0', borderRadius: '25px', margin: '20px', height: '100%', textAlign: 'center' }}>
-                    <Stack>
-                        <img src={logo} alt='Swargadhi logo' style={{ margin: '20px' }} />
-                        <Typography variant='h4' style={{ margin: '20px' }}>Admin Panel</Typography>
-                        <Stack direction='column' gap='50px' width='80%' justifyContent='center' alignContent='center' margin='auto'>
-                            <Button variant='contained' color='success'>Dashboard</Button>
-                            <Button variant='contained' color='success'>Admin Profile</Button>
-                            <Button variant='contained' color='success'>Orders</Button>
-                            <Button variant='contained' color='success'>Products</Button>
-                            <Button variant='contained' color='success'>Sales Report</Button>
-                            <Button variant='contained' color='success'>Users</Button>
-                            <Button variant='contained' color='success'>Messages</Button>
-                            <Button variant='contained' color='success'>Settings</Button>
-                            <Button variant='contained' color='success'>Sign Out</Button>
-                        </Stack>
-                    </Stack>
-                </Box>
-            </Grid> */}
             <Grid item xs={9}>
                 <Box sx={{ backgroundColor: '#C8FFDB', borderRadius: '25px', margin: '20px', height: '100%' }}>
                     <Stack>
