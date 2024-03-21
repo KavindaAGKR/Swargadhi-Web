bed6c6b1cc62c3a8ac7a5c5abb96fef5f7e56f26
chanuka <chanukawelagedara@gmail.com>
2024-03-21 08:48:51 +0530

    27
    adding the cart model

---
+++ Backend/models/cartModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema(
+  {
+    user:{
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    product:{
+      type: mongoose.Schema.Types.ObjectId,
+      require: true
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number, 
+      required: true,
+    },
+    images:{
+      type:[String]
+    },
+  
+    },
+    // image: {
+    //   type: String,
+    // },
+  
+  {
+    timestamps: true,
+  }
+);
+
+
+export const cartModel = mongoose.model("Cart", cartSchema);
