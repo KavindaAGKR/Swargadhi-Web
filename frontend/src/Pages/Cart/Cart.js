934f255e0b1c8456def13d04dc06c0746d80f14c
KavindaAGKR <kavindaaludeniya@gmail.com>
2024-03-26 00:33:47 +0530

    Cart page completed with dummy data

--- frontend/src/Pages/Cart/Cart.js
+++ frontend/src/Pages/Cart/Cart.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { Header } from '../../Components/Header'
 import { Footer } from '../../Components/Footer'
 
-import {Stack ,  Typography } from '@mui/material'
+import {Grid, Paper, Stack ,  Typography, Button } from '@mui/material'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { CartCard } from './CartCard';
-import { Box } from '@mui/system';
-import { Button } from '@mui/base';
+
+
+
+
+
+
 
 export const Cart = () => {
     return (
@@ -15,7 +19,7 @@ export const Cart = () => {
         <Stack>
             <Typography textAlign='center' variant='h3' padding='25px'> <ShoppingCartIcon fontSize='30' color='red'/> My Cart</Typography>
 
-            <Stack direction='row' padding='25px'>
+            <Stack direction='row' padding='20px'>
 
                 <Stack sx={{width:'70%'}} justifyContent='center' alignItems='center' spacing={5}>
                 <CartCard/>
@@ -24,12 +28,42 @@ export const Cart = () => {
                 <CartCard/>
                 <CartCard/>
                 </Stack>
-                <Stack>
-                    <Typography>Sub Total: 7899</Typography>
-                    <Typography>Delivary Charges: 450</Typography>
-                    <Typography>Discount</Typography>
-                    <Typography>Total Cost</Typography>
-                    <Button>Proceed to Checkout</Button>
+                <Stack sx={{width:'30%'}} >
+                    <Paper elevation={5} sx={{padding:'50px'}}  >
+                    <Grid container spacing={2}  >
+  <Grid item xs={8} md={8}>
+  <Typography variant='h6'>Sub Total</Typography>
+  </Grid>
+  <Grid item xs={4} md={4}>
+  <Typography variant='h6'>7899</Typography>
+  </Grid>
+  <Grid item xs={8} md={8}>
+  <Typography variant='h6'>Delivary Charges</Typography>
+  </Grid>
+  <Grid item xs={4} md={4}>
+  <Typography variant='h6'>450</Typography>
+  </Grid>
+  <Grid item xs={8} md={8}>
+  <Typography variant='h6'>Discount</Typography>
+  </Grid>
+  <Grid item xs={4} md={4}>
+  <Typography variant='h6'>10%</Typography>
+  </Grid>
+  <Grid item xs={8} md={8}>
+  <Typography variant='h5'>Total Cost</Typography>
+  </Grid>
+  <Grid item xs={4} md={4}>
+  <Typography variant='h5'>8000</Typography>
+  </Grid>
+</Grid>
+
+
+
+                    
+<Stack margin='25px auto ' width='60%'>
+                    <Button variant='contained' >Proceed to Checkout</Button>
+                    </Stack>
+                    </Paper>
                 </Stack>
             </Stack>
         </Stack>
