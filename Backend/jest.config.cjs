9fb27252c501734f0fbc32c044ca42d6ceebbd57
Kavinda Aludeniya <aludeniya_agkr_e22@engug.ruh.ac.lk>
2024-06-27 02:01:10 +0530

    Delete Backend directory

--- Backend/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-const { defaults } = require('jest-config');
-
-module.exports = {
-  ...defaults,
-  testMatch: ['<rootDir>/Test/**/*.test.js'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-};
